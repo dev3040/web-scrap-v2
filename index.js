a61c03f82e15c647923ecab1f87f94398af9f8a9
Devang Patel <devangp@itpathsolutions.com>
2024-01-01 19:58:24 +0530

    changes

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ app.use(cookieParser());
 
 // Function to extract _token and cookies from the HTML response
 async function scrapeAttendanceData(username, password, date) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
 
   try {
